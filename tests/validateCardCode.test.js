67fbce06bedd62f7e80517eb10f06877e0bdfb88
ania221B <100519699+ania221B@users.noreply.github.com>
2024-12-02 23:11:51 +0100

    tests for validateCardCode added

---
+++ tests/validateCardCode.test.js
@@ -0,0 +1,71 @@
+import { validate } from 'jest-validate'
+import { validateCardCode } from '../js/main'
+
+describe('validateCardCode', () => {
+  let cardNumberInput
+  let cardCodeInput
+  let cardCodeLabel
+  let cardCodeMinLength
+  let cardCodeMaxLength
+
+  beforeEach(() => {
+    document.body.innerHTML = `<main>
+      <section id="payment-section" class="payment section" hidden>
+      <div class="card payment-card container">
+          <div class="card__img">
+          </div>
+          <div class="card__content flow">
+            <form class="payment__form"> 
+             <h2 class="h2">Payment Details</h2>
+             <div class="form__control-wrapper">
+              <label for="card-number">Card Number:</label>
+              <input id="card-number" class="numeric-input" type="text" data-inputmode="numeric" minlength="13" maxlength="19" pattern="[0-9\s]{13,19}" placeholder="1234 5678 9012 3456" required>
+              <div class="error-message"></div>
+            </div> 
+
+            <div class="form__control-wrapper">
+              <label for="card-code">CVV:</label>
+              <input id="card-code" type="text" minlength="3" maxlength="4" pattern="\d{3,4}" placeholder="CVV" required>
+              <div class="error-message"></div>
+            </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </main>`
+    cardNumberInput = document.querySelector('#card-number')
+    cardCodeInput = document.querySelector('#card-code')
+    cardCodeLabel =
+      cardCodeInput.parentElement.querySelector('label').textContent
+    cardCodeMinLength = cardCodeInput.getAttribute('minlength')
+    cardCodeMaxLength = cardCodeInput.getAttribute('maxlength')
+  })
+
+  test('returns null when card code is valid', () => {
+    cardNumberInput = '1234567891234'
+    cardCodeInput.value = '1234'
+
+    expect(validateCardCode()).toBeNull()
+  })
+  test('returns correct error message when card code is missing', () => {
+    cardCodeInput.value = ''
+    expect(validateCardCode()).toBe(`Please provide ${cardCodeLabel}`)
+  })
+  test('returns correct error message when card code is too short', () => {
+    cardCodeInput.value = '12'
+    expect(validateCardCode()).toBe(
+      `${cardCodeLabel} must have at least ${cardCodeMinLength} digits`
+    )
+  })
+  test('returns correct error message when card code is too long', () => {
+    cardCodeInput.value = '12345'
+    expect(validateCardCode()).toBe(
+      `${cardCodeLabel} must have at most ${cardCodeMaxLength} digits`
+    )
+  })
+  test('returns correct error message when card is American Express and card code has less than 4 digits', () => {
+    cardNumberInput.value = '3712345678901'
+    cardCodeInput.value = '123'
+    expect(validateCardCode()).toBe('Card code must be 4 digits')
+  })
+})
