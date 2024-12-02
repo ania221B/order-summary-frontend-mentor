c486ed3cd16ef379e5a84efe63594bed44be232e
ania221B <100519699+ania221B@users.noreply.github.com>
2024-12-02 23:09:57 +0100

    tests for validateHolderName added

---
+++ tests/validateHolderName.test.js
@@ -0,0 +1,40 @@
+import { validateHolderName } from '../js/main'
+
+describe('validateHolderName', () => {
+  let cardHolderNameInput
+  beforeEach(() => {
+    document.body.innerHTML = `<main>
+      <section id="payment-section" class="payment section" hidden>
+      <div class="card payment-card container">
+          <div class="card__img">
+          </div>
+          <div class="card__content flow">
+            <form class="payment__form"> 
+             <h2 class="h2">Payment Details</h2>
+             <div class="form__control-wrapper">
+              <label for="card-holder-name">Name on card:</label>
+              <input id="card-holder-name" type="text" placeholder="Jane Doe" required>
+              <div class="error-message"></div>
+            </div>   
+            </form>
+          </div>
+        </div>
+      </section>
+    </main>`
+    cardHolderNameInput = document.querySelector('#card-holder-name')
+  })
+  test('returns null when card holder name is valid', () => {
+    cardHolderNameInput.value = 'Anna-Maria Kowalska'
+    expect(validateHolderName()).toBeNull()
+  })
+  test('returns correct error message when card holder name is missing', () => {
+    cardHolderNameInput.value = ''
+    expect(validateHolderName()).toBe('Please provide name on card')
+  })
+  test('returns correct error message when card holder name is invalid', () => {
+    cardHolderNameInput.value = 'abc'
+    expect(validateHolderName()).toBe(
+      'Use letters & hyphens for name & surname'
+    )
+  })
+})
